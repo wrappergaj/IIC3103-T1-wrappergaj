3282c0e5c66b6cca76d825295738d36389e02eaa
wrappergaj <sebastian.gajardo@uc.cl>
2024-04-04 18:13:50 -0300

    avance

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def load_data():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug = True, port = 10000)
+    app.run(debug = True, port = 10000, host="0.0.0.0")
