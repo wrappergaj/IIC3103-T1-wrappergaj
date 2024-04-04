6d464e1aefe2da120390aa26ef8b2c90758454f7
wrappergaj <sebastian.gajardo@uc.cl>
2024-04-04 19:41:30 -0300

    cambios

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def create_user():
         'created_at': new_user.created_at.strftime('%Y-%m-%d %H:%M:%S')
     }
     
-    return jsonify(user_json), 201
+    return user_json, 201
     
 @app.route('/posts', methods = ['GET'])
 def get_posts():
@@ -150,7 +150,7 @@ def create_post():
             'userId': new_post.userId,
             'created_at': new_post.created_at.strftime('%Y-%m-%d %H:%M:%S')
     }
-    return jsonify(post_json), 201
+    return post_json, 201
 
 @app.route('/comments', methods = ['GET'])
 def get_comments():
@@ -183,7 +183,7 @@ def create_comment():
             'userId': new_comment.userId,
             'postId': new_comment.postId
         }
-    return jsonify(comment_json), 201
+    return comment_json, 201
 
 @app.route('/reset', methods=['POST'])
 def delete_all_data():
